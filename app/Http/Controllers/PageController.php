c96c684ae43c19314376c2f2492b05ccddea1025
H <62352013+iHector1@users.noreply.github.com>
2023-07-08 13:29:12 -0600

    migraciones

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,24 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
-class PageController extends Controller
-{
-    public function home(){
+class PageController extends Controller {
+    public function home()
+    {
         return view('home');
     }
 
-    public function blog(){
-         //consulta a base de datos
-        $posts=[
-            ['id'=> 1,'title'=> 'PHP', 'slug'=>'php'],
-            ['id'=> 2,'title'=> 'Laravel', 'slug'=>'laravel']
-        ];
-        return view('blog',['posts'=>$posts]);
+    public function blog()
+    {
+        //consulta a base de datos
+        //$posts = Post::get();
+        // $post = Post::first();]
+        // $post = Post::find(25);
+        //dd($post);
+        $posts = Post::latest()->paginate();
+        return view('blog', ['posts' => $posts]);
     }
 
-    public function post($slug){
-        $post = $slug;
-        return view('post',['post'=>$post]);
+    public function post(Post $post)
+    {
+        return view('post', ['post' => $post]);
     }
 }
