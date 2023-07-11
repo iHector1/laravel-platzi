39b2ff976f39b6476bdb59fd3fcea1c028b0ceed
H <62352013+iHector1@users.noreply.github.com>
2023-07-10 20:18:32 -0600

    actualizacion de rutas y panel administrativo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostController extends Controller
-{
+class PostController extends Controller {
     //
+    public function index()
+    {
+        return view('posts.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
@@ -26,10 +27,6 @@ Route::get('/dashboard', function () {
        return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-       Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-       Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-       Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::resource('posts', PostController::class)->except(['show'])->middleware(['auth', 'verified']);
 
 require __DIR__ . '/auth.php';
