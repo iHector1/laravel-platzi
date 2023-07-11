2d0b4dc355680549729e1e96350d221f89005a1a
H <62352013+iHector1@users.noreply.github.com>
2023-07-10 21:51:27 -0600

    creacion de posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller {
     //
@@ -16,6 +17,15 @@ class PostController extends Controller {
     public function create(Post $post)
     {
         return view('posts.create', ['post' => $post]);
+    }
+    public function store(Request $request)
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug' => Str::slug($title),
+            'body' => $request->body
+        ]);
+        return redirect()->route('posts.edit', $post);
     }
     public function edit(Post $post)
     {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+    ];
 
     public function user()
     {
