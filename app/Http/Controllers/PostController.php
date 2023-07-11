ba90dddec625ab123bcd4e6425b743fd16a65737
H <62352013+iHector1@users.noreply.github.com>
2023-07-10 22:11:21 -0600

    registros duplicados

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,11 +22,12 @@ class PostController extends Controller {
     {
         $request->validate([
             'title' => 'required',
+            'slug' => 'required | unique:posts,slug',
             'body' => 'required'
         ]);
         $post = $request->user()->posts()->create([
-            'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+            'title' => $request->title,
+            'slug' => $request->slug,
             'body' => $request->body
         ]);
         return redirect()->route('posts.edit', $post);
@@ -37,13 +38,15 @@ class PostController extends Controller {
     }
     public function update(Request $request, Post $post)
     {
+        
         $request->validate([
             'title' => 'required',
+            'slug' => 'required | unique:posts,slug,'.$post->id,
             'body' => 'required'
         ]);
         $post->update([
-            'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+            'title' => $request->title,
+            'slug' => $request->slug,
             'body' => $request->body
         ]);
         return redirect()->route('posts.edit', $post);
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -4,6 +4,10 @@
 <span class="text-xs text-red-600">@error('title') {{$message}} @enderror</span>
 
 <input type="text" name="title" class="rounded border-gray-200 w-full mb-4"  value="{{ old('title',$post->title) }}">
+<label class="uppercase text-gray-700 text-xs">Slug</label>
+<span class="text-xs text-red-600">@error('slug') {{$message}} @enderror</span>
+
+<input type="text" name="slug" class="rounded border-gray-200 w-full mb-4"  value="{{ old('slug',$post->slug) }}">
 
 <label class="uppercase text-gray-700 text-xs">Contenido</label><span class="text-xs text-red-600">@error('body') {{$message}} @enderror</span>
 <textarea name="body" class="rounded border-gray-200 w-full mb-4" rows="5">{{ old('body',$post->body) }}</textarea> 
